cbf045abebb8512d3b8276f37c87818f214b4f97
Ylkhana <48254532+Ylkhana@users.noreply.github.com>
2022-08-24 15:40:29 +0200

    Added player movement

--- __main__.py
+++ __main__.py
@@ -1,12 +1,16 @@
-from time import clock_getres
 import pygame, sys
 from settings import *
+from player import Player
 
 # Basic init
 pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Frogger')
 clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+
+player = Player((0,0), all_sprites)
 
 # Game loop
 while True:
@@ -16,5 +20,13 @@ while True:
             sys.exit()
 
     dt = clock.tick(60) / 1000
+
+    display_surface.fill((0,0,0))
+
+    # Update
+    all_sprites.update(dt)
+
+    # Draw
+    all_sprites.draw(display_surface)
 
     pygame.display.update()
