e97ebd0c213d1e4c450d9b12a2d34e58e3a7b733
Ylkhana <48254532+Ylkhana@users.noreply.github.com>
2022-08-24 15:06:44 +0200

    Basic setup

---
+++ __main__.py
@@ -0,0 +1,20 @@
+from time import clock_getres
+import pygame, sys
+from settings import *
+
+# Basic init
+pygame.init()
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Frogger')
+clock = pygame.time.Clock()
+
+# Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    dt = clock.tick(60) / 1000
+
+    pygame.display.update()
