75862540fe80774bb17688afd7dbd8069decc58a
Ylkhana <48254532+Ylkhana@users.noreply.github.com>
2022-08-25 12:22:50 +0200

    Added camera support

--- __main__.py
+++ __main__.py
@@ -1,7 +1,27 @@
+from lib2to3.pgen2 import grammar
 import pygame, sys
 from settings import *
 from player import Player
 from car import Car
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self) -> None:
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.bg = pygame.image.load('graphics/main/map.png').convert()
+        self.fg = pygame.image.load('graphics/main/overlay.png').convert_alpha()
+
+    def custom_draw(self):
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+        display_surface.blit(self.bg, -self.offset)
+
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            display_surface.blit(sprite.image, offset_pos)
+
+        display_surface.blit(self.fg, -self.offset)
 
 # Basic init
 pygame.init()
@@ -9,10 +29,10 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Frogger')
 clock = pygame.time.Clock()
 
-all_sprites = pygame.sprite.Group()
+all_sprites = AllSprites()
     
-player = Player((400,500), all_sprites)
-car = Car((100,200), all_sprites)
+player = Player((600,400), all_sprites)
+car = Car((600,200), all_sprites)
 
 # Game loop
 while True:
@@ -29,6 +49,6 @@ while True:
     all_sprites.update(dt)
 
     # Draw
-    all_sprites.draw(display_surface)
+    all_sprites.custom_draw()
 
     pygame.display.update()
